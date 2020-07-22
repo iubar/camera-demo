0ea45511cc7ecc67f40f6477dfa8cc1b5185ce5c
borgogelli <borgogelli@iubar.it>
2020-07-22 18:54:59 +0200

    .

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import CameraScreen from './src/CameraScreen';
-import CameraExpoExample from './src/CameraExpoExample';
+// import CameraExpoExample from './src/CameraExpoExample';
  
 const Stack = createStackNavigator();
 
+/**
+ * 	@todo: React context and wrap the navigator with a context provider to pass data to the screens (recommended).
+ */
 export default class App extends React.Component {	
-	// TODO: Use React context and wrap the navigator with a context provider to pass data to the screens (recommended).
     render(){
 		return (
 		<NavigationContainer>
